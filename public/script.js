daa55340118a3b40ca91a6fd50e7319242412dcd
baoday97 <161543862+Nguyen-Bao11@users.noreply.github.com>
2026-03-08 09:59:19 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,34 @@
+const input = document.getElementById("input");
+const chat = document.getElementById("chat");
 
+input.addEventListener("keypress", async (e) => {
+
+if(e.key === "Enter"){
+
+const text = input.value;
+
+chat.innerHTML += `<div class="user">You: ${text}</div>`;
+
+input.value="";
+
+const res = await fetch("/chat",{
+
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:JSON.stringify({
+message:text
+})
+
+});
+
+const data = await res.json();
+
+chat.innerHTML += `<div class="bot">AI: ${data.reply}</div>`;
+
+chat.scrollTop = chat.scrollHeight;
+
+}
+
+});
